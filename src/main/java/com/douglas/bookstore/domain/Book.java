70e661d37eb61015a3abfc924ca47686a9c65739
Douglas Oliveira <douglas.toliveira.dev@gmail.com>
2022-11-07 16:41:37 -0300

    Domain classes - Book and Category

---
+++ src/main/java/com/douglas/bookstore/domain/Book.java
@@ -0,0 +1,82 @@
+package com.douglas.bookstore.domain;
+
+import java.util.Objects;
+
+public class Book {
+	private Integer id;
+	private String title;
+	private String author;
+	private String text;
+	
+	private Category category;
+	
+	public Book() {
+		super();
+	}
+
+	public Book(Integer id, String title, String author, String text, Category category) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.text = text;
+		this.category = category;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(id, other.id);
+	}
+}
