4f3998aea9851e15abfcd6eb45666d512afb2172
Douglas Oliveira <douglas.toliveira.dev@gmail.com>
2022-11-07 23:54:55 -0300

    Dev profile created

---
+++ src/main/java/com/douglas/bookstore/config/DevConfig.java
@@ -0,0 +1,28 @@
+package com.douglas.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.douglas.bookstore.service.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean databaseSeeding() {
+		if(strategy.equals("create")) {
+			this.dbService.databaseSeeding();
+		}
+		return false;
+	}
+}
