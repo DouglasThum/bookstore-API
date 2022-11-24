9b444899572a478e81123ae52f6eec4c455d582a
Douglas Oliveira <douglas.toliveira.dev@gmail.com>
2022-11-23 23:12:03 -0300

    CorsConfig

--- src/main/java/com/douglas/bookstore/resources/CategoryResource.java
+++ src/main/java/com/douglas/bookstore/resources/CategoryResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.douglas.bookstore.domain.Category;
 import com.douglas.bookstore.dtos.CategoryDTO;
 import com.douglas.bookstore.service.CategoryService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
